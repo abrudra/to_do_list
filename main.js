938ff8937be554652abd9e541263dadc24178f2f
Birge Atul <atulbirge@gmail.com>
2022-12-17 17:03:49 +0530

    UI Modified and code optimized as possible and functions works properly.

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-
 ////..............Header..............////////
-
 document.body.style.background = "#e6e3e3";
 let headerDiv = document.createElement("div");
 document.body.appendChild(headerDiv);
@@ -16,17 +14,20 @@ headerDiv.style.alignItems = "center";
 headerDiv.style.color = "#f7999e";
 headerDiv.className = "header-class";
 headerDiv.style.flexDirection = "column";
-// headerDiv.style.border ="2px solid red"
 
 //...........Checkbox for all checking.....//
+let checkboxDiv = document.createElement("div");
+headerDiv.appendChild(checkboxDiv);
+checkboxDiv.classList.add("checkbox-Div");
+
 let checkBoxAll = document.createElement("input");
 checkBoxAll.setAttribute("type", "checkbox");
 checkBoxAll.className = "checkboxall-class";
-headerDiv.appendChild(checkBoxAll);
+checkboxDiv.appendChild(checkBoxAll);
 //////..............Inpute Box............./////
 
 let inputBox = document.createElement("input");
-headerDiv.appendChild(inputBox);
+checkboxDiv.appendChild(inputBox);
 inputBox.style.fontSize = "30px";
 inputBox.setAttribute = ("type", "text");
 inputBox.placeholder = "What needs to be done?";
@@ -38,34 +39,24 @@ inputBox.style.boxShadow = "10px 20px 30px #b5b0b0";
 inputBox.className = "css-inputBox";
 inputBox.id = "filter";
 inputBox.style.outline = "none";
+inputBox.style.paddingLeft = "10%";
 
 ///...............Task Div................///
 
 let taskDiv = document.createElement("div");
 document.body.appendChild(taskDiv);
 taskDiv.className = "css-taskdiv";
-// taskDiv.style.border = "2px solid black";
+
 taskDiv.style.height = "100%";
 taskDiv.style.display = "flex";
 taskDiv.style.flexDirection = "column";
-// taskDiv.style.justifyContent =" center"
 
 ////..............ul list.................////
 
 let listul = document.createElement("ul");
 listul.setAttribute("id", "myUl");
 taskDiv.appendChild(listul);
-///listul.style.border = "2px solid black";
-// listul.style.width = "30%"
-listul.style.marginLeft = "30%";
-// listul.style.background = "white"
-listul.style.display = "flex";
-listul.style.flexDirection = "column";
-
-// let clearbtn = document.createElement("button");
-// listul.appendChild(clearbtn);
-// clearbtn.style.height = "2rem";
-// clearbtn.setAttribute("value", "reset form");
+listul.classList.add("listul-class");
 
 //...........All butn  div...........//
 
@@ -81,22 +72,23 @@ counterDiv.innerText = " 0 Item Left";
 let allBtnDiv = document.createElement("div");
 btnDiv.appendChild(allBtnDiv);
 allBtnDiv.classList.add("allbtndiv");
+allBtnDiv.style.display = "flex";
+allBtnDiv.style.gap = "5%";
 
 let allbtn = document.createElement("button");
 allBtnDiv.appendChild(allbtn);
 allbtn.classList.add("allbtn-class", "btncolor");
 allbtn.innerText = "All";
 
-
 let activeBtn = document.createElement("button");
 allBtnDiv.appendChild(activeBtn);
 activeBtn.classList.add("activebtn-class", "btncolor");
-activeBtn.innerText = "active";
+activeBtn.innerText = "Active";
 
 let completeBtn = document.createElement("button");
 allBtnDiv.appendChild(completeBtn);
-completeBtn.classList.add("completeBtn-class","btncolor");
-completeBtn.innerText = "complete";
+completeBtn.classList.add("completeBtn-class", "btncolor");
+completeBtn.innerText = "Complete";
 
 let clearDiv = document.createElement("button");
 btnDiv.appendChild(clearDiv);
@@ -105,103 +97,42 @@ clearDiv.innerText = "Clear Data";
 ///.........Function For adding text........//////
 
 let inputText = document.getElementById("filter");
-// console.log(inputText)
-
-///......Enter btn event for submiting task to li....../
-
-inputText.addEventListener("keypress", addItem);
-inputText.addEventListener("keypress", countItem);
-let checkBoxCount = document.querySelector;
-
-///..........clearing text from input box.....//
-
-inputText.addEventListener("keypress", myFunction);
-
-////.........function for adding......///
-listul.addEventListener("click", removeItem);
 
 //.....adding item........///
-let count = 0;
 
-function addItem(event) {
-  if (event.keyCode == 13) {
-    let newTask = inputText.value;
-     if (newTask !== "") {
-    var data = JSON.parse(localStorage.getItem("data"));
-    if (data === null) {
-      localStorage.setItem(
-        "data",
-        JSON.stringify([{ message: event.target.value }])
-      );
-    } else {
-      var data = JSON.parse(localStorage.getItem("data"));
-      const arrayData = [...data, { message: event.target.value }];
-      localStorage.setItem("data", JSON.stringify(arrayData));
-    }
-    var myList = document.getElementById("myUl");
-    myList.innerHTML = "";
-    demo();
-    event.preventDefault();
-    count++;
-    //........geting input.........//
-    
-   
-
-    //.......creating new element.....//
-
-  }/////check / checked..../
-  }
-}
-
-///..........Romoving Item Function.........//
-const filterData = (data, optionId) => {
-  return data.filter((item, index) => {
-    if (index !== optionId) {
-      return item;
-    }
-  });
-};
-function removeItem(event) {
-  var data = JSON.parse(localStorage.getItem("data"));
-  if (event.target.classList.contains("btn-class")) {
-    let li = event.target.parentElement;
-    var ret = li.id;
-    const optionId = ret.replace(/\D/g, "") - 1;
-    const dataToPushLocalstorage = filterData(data, optionId);
-    localStorage.setItem("data", JSON.stringify(dataToPushLocalstorage));
-    listul.removeChild(li);
-    countItem();
-  }
-}
-
-function myFunction(e) {
-  if (e.keyCode == 13) {
-    e.preventDefault();
-
-    let clearValue = document.getElementById("filter");
-    clearValue.value = "";
-  }
-}
-
-///......................Cheking All................./////
-checkBoxAll.addEventListener("change", (e) => {
-  let checkBoxSelect = document.querySelectorAll(".checkbox-class");
-  let lineThrow = taskDiv.firstElementChild.childNodes;
-  lineThrow.forEach((ele) => (ele.style.textDecoration = "line-through"));
-
-  if (checkBoxAll.checked) {
-    checkBoxSelect.forEach((ele) => (ele.checked = true));
-    lineThrow.forEach((ele) => (ele.style.textDecoration = "line-through"));
-  } else {
-    checkBoxSelect.forEach((ele) => (ele.checked = false));
-    lineThrow.forEach((ele) => (ele.style.textDecoration = "none"));
-  }
+inputText.addEventListener("keypress", (event) => {
+  addItem(event);
   countItem();
 });
+//.....Add Item..............Enter btn event for submiting task to li....
+let addItem = (event) => {
+  if (event.keyCode == 13) {
+    event.preventDefault();
+    let newTask = inputText.value;
+    if (newTask !== "") {
+      let data = JSON.parse(localStorage.getItem("data"));
+      if (data === null) {
+        localStorage.setItem(
+          "data",
+          JSON.stringify([{ message: event.target.value }])
+        );
+      } else {
+        let data = JSON.parse(localStorage.getItem("data"));
+        const arrayData = [...data, { message: event.target.value }];
+        localStorage.setItem("data", JSON.stringify(arrayData));
+      }
+      let myList = document.getElementById("myUl");
+      myList.innerHTML = "";
+      let clearValue = document.getElementById("filter");
+      clearValue.value = "";
+
+      localData();
+    }
+  }
+};
 
 ////...........Adding Task count................//////
-
-function countItem(event) {
+let countItem = () => {
   let startCount = document.querySelector(".counterdiv-class");
   let newCount = 0;
 
@@ -212,9 +143,46 @@ function countItem(event) {
     }
   });
   startCount.innerText = newCount + " Items Left";
-}
+};
 
-////..........applying button to function.....///
+///..........Romoving Item Function.........//
+
+const filterData = (data, optionId) => {
+  return data.filter((item, index) => {
+    if (index !== optionId) {
+      return item;
+    }
+  });
+};
+listul.addEventListener("click", (event) => {
+  let data = JSON.parse(localStorage.getItem("data"));
+  if (event.target.classList.contains("btn-class")) {
+    let li = event.target.parentElement;
+    let ret = li.id;
+    const optionId = ret.replace(/\D/g, "") - 1;
+    const dataToPushLocalstorage = filterData(data, optionId);
+    localStorage.setItem("data", JSON.stringify(dataToPushLocalstorage));
+    listul.removeChild(li);
+    countItem();
+  }
+});
+
+///......................Cheking All................./////
+checkBoxAll.addEventListener("change", (e) => {
+  let checkBoxSelect = document.querySelectorAll(".checkbox-class");
+  let lineThrow = taskDiv.firstElementChild.childNodes;
+  lineThrow.forEach((ele) => (ele.style.textDecoration = "line-through"));
+  if (checkBoxAll.checked) {
+    checkBoxSelect.forEach((ele) => (ele.checked = true));
+    lineThrow.forEach((ele) => (ele.style.textDecoration = "line-through"));
+  } else {
+    checkBoxSelect.forEach((ele) => (ele.checked = false));
+    lineThrow.forEach((ele) => (ele.style.textDecoration = "none"));
+  }
+
+  countItem();
+});
+///..........applying button to function.....///
 
 let allTaskShow = document.querySelector(".allbtn-class");
 allTaskShow.addEventListener("click", showTask);
@@ -228,7 +196,6 @@ function showTask(event) {
 let activeshow = document.querySelector(".activebtn-class");
 activeshow.addEventListener("click", showActiveTask);
 function showActiveTask(event) {
-  // let taskContain = taskDiv.firstElementChild.childNodes;
   let taskContain = document.querySelectorAll(".checkbox-class");
   taskContain.forEach((ele) => {
     if (ele.checked) {
@@ -242,7 +209,6 @@ function showActiveTask(event) {
 let completedShow = document.querySelector(".completeBtn-class");
 completedShow.addEventListener("click", completedShowTask);
 function completedShowTask(event) {
-  // let taskContain = taskDiv.firstElementChild.childNodes;
   let taskContain = document.querySelectorAll(".checkbox-class");
 
   taskContain.forEach((ele) => {
@@ -255,8 +221,7 @@ function completedShowTask(event) {
 }
 
 let cleardataword = document.querySelector(".cleardiv-class");
-cleardataword.addEventListener("click", deletedata);
-function deletedata(event) {
+cleardataword.addEventListener("click", () => {
   let taskContain = document.querySelectorAll(".checkbox-class");
   taskContain.forEach((ele) => {
     ele.parentElement.remove();
@@ -264,23 +229,20 @@ function deletedata(event) {
   countItem();
   localStorage.clear();
   let uncheck = document.querySelector(".checkboxall-class");
-  if(uncheck.checked == true){
+  if (uncheck.checked == true) {
     uncheck.checked = false;
-  }else{
+  } else {
     uncheck.checked = false;
   }
-}
-function demo() {
-  var data = JSON.parse(localStorage.getItem("data"));
+});
+
+function localData() {
+  let data = JSON.parse(localStorage.getItem("data"));
 
   if (data !== null) {
     let count = 0;
-    
+
     data.forEach(function (item) {
-      //var li = document.createElement("li");
-      //var text = document.createTextNode(item.message);
-      //li.appendChild(text);
-  
       let li = document.createElement("li");
       count++;
       li.id = "liid" + count;
@@ -295,21 +257,19 @@ function demo() {
       li.appendChild(checkBox);
       listul.appendChild(li);
       li.appendChild(btnCancel);
-         
+
       checkBox.addEventListener("change", (e) => {
-        // let lineThrow = document.getElementsByClassName("li-class");
         if (checkBox.checked) {
           e.target.parentElement.style.textDecoration = "line-through";
         } else {
           e.target.parentElement.style.textDecoration = "none";
         }
       });
-      //li.addEventListener("click", test);
-     
 
       checkBox.addEventListener("click", countItem);
       document.getElementById("myUl").appendChild(li);
+      countItem();
     });
   }
 }
-demo();
+localData();
